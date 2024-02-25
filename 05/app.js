3c5a9497f4df0bcd020d5fa26c93dbe5cab63f38
Kamil Lewandowski <kamlew555@gmail.com>
2024-02-25 18:30:11 +0100

    Add validation to prevent adding empty user data

--- 05/app.js
+++ 05/app.js
@@ -1,42 +1,3 @@
-// const apiUrl = 'http://localhost:3000/users';
-
-// document.addEventListener('DOMContentLoaded', init);
-
-// function init() {
-//     loadUsers();
-// }
-
-// function loadUsers() {
-//     const promise = fetchGet(apiUrl);
-
-//     promise
-//         .then(data => insertUsers(data))
-//         .catch(err => console.error(err));
-// }
-
-// function fetchGet(url) {
-//     return fetch(url)
-//         .then(resp => {
-//             if(resp.ok) {
-//                 return resp.json();
-//             }
-
-//             return Promise.reject(resp);
-//         });
-// }
-
-// function insertUsers(usersList) {
-//     const ulElement = document.querySelector('.users');
-//     ulElement.innerHTML = '';
-//     usersList.forEach(user => {
-//         const liElement = document.createElement('li');
-//         liElement.innerText = `${user.firstName} ${user.lastName}`;
-
-//         ulElement.appendChild(liElement);
-//     });
-// }
-
-
 const apiUrl = "http://localhost:3000/users";
 
 document.addEventListener("DOMContentLoaded", init);
@@ -82,6 +43,10 @@ function setupForm() {
 		const firstNameInput = document.querySelector(".form__field--first-name");
 		const lastNameInput = document.querySelector(".form__field--last-name");
 
+		if(firstNameInput.value === "" || lastNameInput.value === "") {
+			alert("Please enter both first and last name");
+			return;
+		}
 		const newUser = {
 			firstName: firstNameInput.value,
 			lastName: lastNameInput.value,
